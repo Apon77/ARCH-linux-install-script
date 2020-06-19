3bb11cfed4e7ea7e6d596a30dfd28ec0f879c70b
Apon77 <61594531+Apon77@users.noreply.github.com>
2020-06-19 13:25:58 +0600

    Adding no confirm reflector

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 loadkeys dvorak
 setfont Lat2-Terminus16.psfu.gz -m 8859-2
 pacman -Syyy
-pacman -S reflector
+pacman -S --noconfirm reflector
 #Change Bangladesh to your country in next line.
 reflector -c Bangladesh -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyy
